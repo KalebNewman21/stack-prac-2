fc64c1d3bb74e907ec1c2d4cf762ba5d64fb42f9
Kaleb Newman <kalebnewman@Kalebs-MacBook-Air.local>
2021-08-21 15:05:54 -0400

    A new way

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(express.json());
 
 
 
-app.use(express.static(path.join(__dirname, './build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build'))
-})
+//app.use(express.static(path.join(__dirname, './build')))
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, './build'))
+//})
 app.use("/", router);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
